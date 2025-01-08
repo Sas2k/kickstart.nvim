7ba3ef519b7fbff09d1088e90018bde1d580b2aa
Sas2k <sasen.learnings@gmail.com>
2025-01-08 14:13:01 +0530

    .

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,16 +4,16 @@
 -- See the kickstart.nvim README for more information
 
 return {
-    {
-        "HiPhish/rainbow-delimiters.nvim"
-    },
-    {
-      "lervag/vimtex",
-      lazy = false    -- we don't want to lazy load VimTeX
-      -- tag = "v2.15", -- uncomment to pin to a specific release
-      -- init = function()
-        -- VimTeX configuration goes here, e.g.
-        -- vim.g.vimtex_view_method = "zathura"
-      -- end
-    }
+  {
+    'HiPhish/rainbow-delimiters.nvim',
+  },
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    -- init = function()
+    -- VimTeX configuration goes here, e.g.
+    -- vim.g.vimtex_view_method = "zathura"
+    -- end
+  },
 }
