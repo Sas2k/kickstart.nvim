fc1a14f4be73b17fb840b508836c2f089e721999
Sas2k <sasen.learnings@gmail.com>
2025-01-08 13:43:50 +0530

    Updated to add 2 plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,18 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+return {
+    {
+        "HiPhish/rainbow-delimiters.nvim"
+    },
+    {
+      "lervag/vimtex",
+      lazy = false    -- we don't want to lazy load VimTeX
+      -- tag = "v2.15", -- uncomment to pin to a specific release
+      -- init = function()
+        -- VimTeX configuration goes here, e.g.
+        -- vim.g.vimtex_view_method = "zathura"
+      -- end
+    }
+}
